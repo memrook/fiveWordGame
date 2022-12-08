54c068d3dd09c3844fa11d84d62931bd435eb0bc
memrook <memrook@gmail.com>
2022-12-08 12:27:41 +0300

    It's working!
    Took 2 hours 33 minutes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
 	"log"
 	"math/rand"
 	"os"
@@ -10,8 +11,12 @@ import (
 	"time"
 )
 
-const wordbook = "russian_nouns.txt"
+const wordbook = "wordbook.txt"
 const wordLen = 5
+
+var colorLime = color.New(color.FgHiGreen).SprintFunc()
+var colorYellow = color.New(color.FgYellow).SprintFunc()
+var colorBgYellow = color.New(color.BgYellow).SprintFunc()
 
 func main() {
 	file, err := os.Open(wordbook)
@@ -47,18 +52,22 @@ func main() {
 	secretWord := words[randomNr]
 
 	//log.Println(wordCount, randomNr, secretWord)
-	log.Printf("Правила игры просты:\nЯ загадываю слово из %d букв - вы отгадываете.\n"+
-		"В ответе: Буквы в верхнев регистре - угадали в правильном месте", wordLen)
+	fmt.Printf(colorLime("Правила игры просты:\nЯ загадываю слово из %d букв - вы отгадываете.\n"+
+		"В ответе:\n"+
+		"\t-буквы в верхнем регистре - угадали в правильном месте\n"+
+		"\t-буквы в нижнем регистре - они точно есть в слове, но не в этом месте\n"), wordLen)
 
 	var outputWord []string
+	var excludedChars []string
 	var input string
 
 	for fmt.Scanf("%s", &input); input != secretWord; fmt.Scanf("%s", &input) {
 		input = strings.ToLower(input)
 		inputRunes := []rune(input)[:wordLen]
 		secretRunes := []rune(secretWord)
+		outputWord = nil
 
-		log.Println("Вы ввели: ", string(inputRunes))
+		//fmt.Println("Вы ввели: ", string(inputRunes))
 
 		for i, inputChar := range inputRunes {
 			include := 0
@@ -71,14 +80,26 @@ func main() {
 			}
 			switch {
 			case inputChar == secretRunes[i]:
-				outputWord = append(outputWord, strings.ToUpper(string(inputChar)))
+				outputWord = append(outputWord, colorLime(strings.ToUpper(string(inputChar))))
 			case include > 0 && inputChar != secretRunes[i]:
-				outputWord = append(outputWord, string(inputChar))
+				outputWord = append(outputWord, colorYellow(string(inputChar)))
 			default:
 				outputWord = append(outputWord, "*")
+				excludedChars = appendIfNotExists(excludedChars, colorBgYellow(string(inputChar)))
+				//excludedChars = append(excludedChars, colorBgYellow(string(inputChar)))
 			}
 		}
+		fmt.Printf("%v\t\t ❌  Исключенные символы:%v\n", outputWord, excludedChars)
 	}
 	fmt.Println("ВЫ ПОБЕДИЛИ!!!")
 
+}
+
+func appendIfNotExists(origin []string, new string) []string {
+	for _, char := range origin {
+		if char == new {
+			return origin
+		}
+	}
+	return append(origin, new)
 }
